88cfa7a887cff4134a2ecc0d9d106881b5708471
tomaka <htomaka@treeptik.fr>
2017-09-13 22:05:11 +0200

    ngWebWorker

---
+++ packages/ngWebWorker/src/WorkerService.js
@@ -0,0 +1,30 @@
+/**
+ * Created by htomaka on 13/09/17.
+ */
+
+
+function WorkerService($q){
+    let workerPath = '';
+    let worker = new Worker(workerPath);
+    let defer = $q.defer();
+
+    function setWorkerPath(path){
+        workerPath = path;
+    }
+
+    worker.addEventListener('message', function(e) {
+        defer.resolve(e.data);
+    }, false);
+
+    return {
+        processData : function(data){
+            defer = $q.defer();
+            worker.postMessage({
+                'myData': data
+            });
+            return defer.promise;
+        }
+    };
+
+
+}
